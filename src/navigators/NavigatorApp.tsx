1211c944544a6c1cffcbe9967dd49cd25d0478f7
Lương Văn Tú <luongvantuit@gmail.com>
2021-11-10 17:42:43 +0700

    config navigation app

---
+++ src/navigators/NavigatorApp.tsx
@@ -0,0 +1,24 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react';
+import Confirmation from '../screens/Confirmation';
+import OnBroading from '../screens/OnBroading';
+import Recovery from '../screens/Recovery';
+import SignIn from '../screens/SignIn';
+import SignUp from '../screens/SignUp';
+
+const Stack = createNativeStackNavigator();
+
+export default function NavigatorApp() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='OnBroading'>
+        <Stack.Screen name={"SignUp"} component={SignUp} />
+        <Stack.Screen name={"SignIn"} component={SignIn} />
+        <Stack.Screen name={"Recovery"} component={Recovery} />
+        <Stack.Screen name={"Confirmation"} component={Confirmation} />
+        <Stack.Screen name={"OnBroading"} component={OnBroading} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
